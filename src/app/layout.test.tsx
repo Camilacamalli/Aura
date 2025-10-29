df3953fb7ea05f36d73e2b4df1ebfd6197503151
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-10-29 20:35:08 -0300

    unit-red: layout renders Header

---
+++ src/app/layout.test.tsx
@@ -0,0 +1,21 @@
+import { test, expect, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import RootLayout from './layout';
+import * as navigation from 'next/navigation';
+
+vi.mock('next/navigation');
+
+test(" The root layout renders the Header component with homepage content when displaying in homepage", () => {
+  vi.mocked(navigation.usePathname).mockReturnValue('/');
+
+  const MockHomePage = () => <div>Welcome to Homepage</div>;
+
+  render(
+    <RootLayout>
+      <MockHomePage />
+    </RootLayout>
+  )
+
+  expect(() => screen.getByRole('heading', { name: /MoodSelector/i })).not.toThrow()
+})
