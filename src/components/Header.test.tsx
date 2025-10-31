80d9828dd60f4f39aa82c9e8585e1cf0c470b2ca
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-10-31 15:11:13 -0300

    unit-blue

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -11,34 +11,43 @@ describe('Header component behaviour', () => {
     vi.resetAllMocks();
   })
 
-  test("It renders a header element", () => {
+  test('It renders a header element', () => {
     vi.mocked(navigation.usePathname).mockReturnValue('/');
     render(<Header />);
     expect(screen.getByRole("banner")).toBeInTheDocument();
   })
 
-  test("It displays the title MoodSelector when on the homepage", () => {
-    vi.mocked(navigation.usePathname).mockReturnValue('/');
-    render(<Header />);
-    expect(screen.getByRole('heading', { name: /MoodSelector/i })).toBeInTheDocument();
+  describe('When on the homepage...', () => {
+
+    beforeEach(() => {
+      vi.mocked(navigation.usePathname).mockReturnValue('/');
+      render(<Header />)
+    })
+
+    test('...it displays the title MoodSelector', () => {
+      expect(screen.getByRole('heading', { name: /MoodSelector/i })).toBeInTheDocument();
+    })
+
+    test('...it displays the How to use Aura button', () => {
+      expect(screen.getByRole('button', { name: /How to use Aura/i })).toBeInTheDocument();
+    })
+
   })
 
-  test('It displays the title MoodResults when on the /results page ', () => {
-    vi.mocked(navigation.usePathname).mockReturnValue('/results');
-    render(<Header />);
-    expect(screen.getByRole("heading", { name: /MoodResults/i })).toBeInTheDocument();
-  })
+  describe('When on /results page...', () => {
 
-  test('It displays the How to use Aura button when on the homepage', () => {
-    vi.mocked(navigation.usePathname).mockReturnValue('/');
-    render(<Header />);
-    expect(screen.getByRole('button', { name: /How to use Aura/i })).toBeInTheDocument();
-  })
+    beforeEach(() => {
+      vi.mocked(navigation.usePathname).mockReturnValue('/results');
+      render(<Header />)
+    })
 
-  test('It displays a Home button when on the /results page', () => {
-    vi.mocked(navigation.usePathname).mockReturnValue('/results');
-    render(<Header />);
-    expect(screen.getByRole('button', { name: /Home/i })).toBeInTheDocument();
+    test('...it displays the title MoodResults', () => {
+      expect(screen.getByRole("heading", { name: /MoodResults/i })).toBeInTheDocument();
+    })
+
+    test('...it displays a Home button', () => {
+      expect(screen.getByRole('button', { name: /Home/i })).toBeInTheDocument();
+    })
   })
 
 })
