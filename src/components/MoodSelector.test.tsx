e12b18ddbb18faa3f35490cc9ad01ff7e6b330df
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-03 15:14:44 -0300

    unit-blue

--- src/components/MoodSelector.test.tsx
+++ src/components/MoodSelector.test.tsx
@@ -1,18 +1,27 @@
 import { render, screen } from '@testing-library/react';
-import { test, expect, vi } from 'vitest';
+import { describe, test, expect, vi, afterEach } from 'vitest';
 import MoodSelector from '@/components/MoodSelector'
 
-test("It renders a heading with 'What is your Mood today?'", () => {
-  render(<MoodSelector />)
-  expect(screen.getByRole('heading', { name: /What is your Mood today?/i })).toBeInTheDocument();
-})
+describe("MoodSelector component", () => {
 
-test("It displays the current date formatted as 'Weekday, Month Day' ", () => {
-  vi.useFakeTimers();
-  const mockDate = new Date('2025-10-30T10:00:00Z');
-  vi.setSystemTime(mockDate);
+  afterEach(() => {
+    vi.useRealTimers();
+  })
 
-  render(<MoodSelector />);
-  expect(screen.getByText('Thursday, October 30')).toBeInTheDocument();
-  vi.useRealTimers();
-})
+  test("It renders a heading with 'What is your Mood today?'", () => {
+    render(<MoodSelector />)
+    expect(screen.getByRole('heading', { name: /What is your Mood today?/i })).toBeInTheDocument();
+  });
+
+  test("It displays the current date formatted as 'Weekday, Month Day' ", () => {
+    vi.useFakeTimers();
+    const mockDate = new Date('2025-10-30T10:00:00Z');
+    vi.setSystemTime(mockDate);
+    render(<MoodSelector />);
+    expect(screen.getByText('Thursday, October 30')).toBeInTheDocument();
+  });
+
+});
+
+
+
