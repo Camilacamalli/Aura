4d5a0e7be3fcca1c7bb53c5b2edb358c65a1fde9
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-03 15:05:48 -0300

    unit-red: MoodSelector displays current date

--- src/components/MoodSelector.test.tsx
+++ src/components/MoodSelector.test.tsx
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
-import { test, expect } from 'vitest';
+import { test, expect, vi } from 'vitest';
 import MoodSelector from '@/components/MoodSelector'
 
 test("It renders a heading with 'What is your Mood today?'", () => {
   render(<MoodSelector />)
   expect(screen.getByRole('heading', { name: /What is your Mood today?/i })).toBeInTheDocument();
+})
+
+test("It displays the current date formatted as 'Weekday, Month Day' ", () => {
+  vi.useFakeTimers();
+  const mockDate = new Date('2025-10-30T10:00:00Z');
+  vi.setSystemTime(mockDate);
+
+  render(<MoodSelector />);
+  expect(screen.getByText('Thursday, October 30')).toBeInTheDocument();
+  vi.useRealTimers();
 })
