1f8100a931b0165010a339e45ef9115b49f3f75a
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-03 15:08:42 -0300

    unit-green

--- src/components/MoodSelector.tsx
+++ src/components/MoodSelector.tsx
@@ -1,7 +1,16 @@
 export default function MoodSelector() {
+  const today = new Date();
+
+  const formattedDate = new Intl.DateTimeFormat('en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+  }).format(today)
+
   return (
     <div>
       <h1>What is your mood today?</h1>
+      <p>{formattedDate}</p>
     </div>
   )
 }
