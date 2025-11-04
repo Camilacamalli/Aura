1496860d25bbb7e8da8706f33c35c45668f98851
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-04 13:02:14 -0300

    unit-blue

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -1,15 +1,26 @@
-import { test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { test, describe, beforeEach } from 'vitest';
+import { render, screen, within } from '@testing-library/react';
 import Footer from '@/components/Footer';
 
-test("It renders the footer", () => {
-  render(<Footer />);
-  expect(screen.getByRole('contentinfo')).toBeInTheDocument();
+describe("Footer component", () => {
+
+  let footer: HTMLElement;
+
+  beforeEach(() => {
+    render(<Footer />)
+    footer = screen.getByRole('contentinfo');
+  })
+
+  test("It renders the footer element", () => {
+    expect(footer).toBeInTheDocument();
+  })
+
+  test("It displays the copyright notice with the current year", () => {
+    const currentYear = new Date().getFullYear();
+    const copyrightRegex = new RegExp(`© ${currentYear} Aura. All Rights Reserved.`, 'i');
+    expect(within(footer).getByText(copyrightRegex))
+  })
+
 })
 
-test("It displays the copyright notice with the current year", () => {
-  render(<Footer />);
-  const currentYear = new Date().getFullYear();
-  const copyrightRegex = new RegExp(`© ${currentYear} Aura. All Rights Reserved.`, 'i');
-  expect(screen.getByText(copyrightRegex)).toBeInTheDocument();
-})
+
