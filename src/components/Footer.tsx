254ea9be15ee99d41a36c517713d138a9e31a91d
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-04 14:34:53 -0300

    unit-green

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link';
+
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer role="contentinfo">
+      <div>
+        <Link href="/privacy-policy">Privacy Policy</Link>
+      </div>
       <p>&copy; {currentYear} Aura. All Rights Reserved.</p>
     </footer>
   )
