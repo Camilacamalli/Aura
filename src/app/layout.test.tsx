462e0b4cf12c59c557d99f25afdda28434be068e
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-04 13:25:40 -0300

    unit-blue

--- src/app/layout.test.tsx
+++ src/app/layout.test.tsx
@@ -1,4 +1,4 @@
-import { test, expect, vi } from 'vitest';
+import { test, expect, vi, beforeEach, afterEach } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
 
 import RootLayout from './layout';
@@ -6,30 +6,30 @@ import * as navigation from 'next/navigation';
 
 vi.mock('next/navigation');
 
-test(" The root layout renders the Header component with homepage content when displaying in homepage", () => {
-  vi.mocked(navigation.usePathname).mockReturnValue('/');
+describe("RootLayout...", () => {
 
-  const MockHomePage = () => <div>Welcome to Homepage</div>;
+  beforeEach(() => {
+    vi.mocked(navigation.usePathname).mockReturnValue('/');
 
-  render(
-    <RootLayout>
-      <MockHomePage />
-    </RootLayout>
-  )
+    render(
+      <RootLayout>
+        <div>Welcome to Homepage</div>
+      </RootLayout>
+    )
+  })
 
-  expect(() => screen.getByRole('heading', { name: /MoodSelector/i })).not.toThrow();
-  expect(() => screen.getByText(/Welcome to Homepage/i)).not.toThrow();
-  vi.resetAllMocks();
+  afterEach(() => {
+    vi.resetAllMocks();
+  })
+
+  test("...renders the Header component with homepage content when displaying in homepage", () => {
+    expect(() => screen.getByRole('heading', { name: /MoodSelector/i })).not.toThrow();
+    expect(() => screen.getByText(/Welcome to Homepage/i)).not.toThrow();
+  })
+
+  test("...renders the Footer component with footer content", () => {
+    expect(within(screen.getByRole('contentinfo')).getByText(/Aura. All Rights Reserved./i))
+  })
 })
 
-test("RootLayout renders the Footer component with footer content", () => {
-  vi.mocked(navigation.usePathname).mockReturnValue('/');
 
-  render(
-    <RootLayout>
-      <div>Mock Page content</div>
-    </RootLayout>
-  )
-
-  expect(within(screen.getByRole('contentinfo')).getByText(/Aura. All Rights Reserved./i))
-})
