cf49f849400fc4c0e75cf571e0862c7b07b638fc
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-04 14:42:56 -0300

    unit-green

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,6 +7,7 @@ export default function Footer() {
     <footer role="contentinfo">
       <div>
         <Link href="/privacy-policy">Privacy Policy</Link>
+        <Link href="/terms-of-service">Terms of Service</Link>
       </div>
       <p>&copy; {currentYear} Aura. All Rights Reserved.</p>
     </footer>
