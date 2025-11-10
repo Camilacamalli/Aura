2f99edc1360664c2a8d8ab1732febcc328ee305d
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-10 13:32:28 -0300

    unit-red GET returns status 200 when mood is provided

--- src/app/api/songs/route.test.ts
+++ src/app/api/songs/route.test.ts
@@ -11,3 +11,12 @@ test("GET api/songs returns status 400 when mood parameter is missing", async ()
   expect(response.status).toBe(400);
   expect(body).toEqual({ error: "Mood parameter is required" });
 })
+
+test("GET api/songs returns status 200 when a mood is provided", async () => {
+  const request = new NextRequest('http://localhost:3000/api/songs?mood=happy');
+  const response = await GET(request);
+  const body = await response.json();
+
+  expect(response.status).toBe(200);
+  expect(body).toEqual({ songs: [] })
+})
