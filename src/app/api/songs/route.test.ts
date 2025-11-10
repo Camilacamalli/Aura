0ac72a4fa41154919667ee3c59a28ea07d2aa4dd
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-10 13:33:49 -0300

    unit-blue

--- src/app/api/songs/route.test.ts
+++ src/app/api/songs/route.test.ts
@@ -1,22 +1,26 @@
 import { NextRequest } from 'next/server';
-import { test } from 'vitest';
+import { test, describe, expect } from 'vitest';
 import { GET } from './route'
 
+describe("GET api/songs", () => {
 
-test("GET api/songs returns status 400 when mood parameter is missing", async () => {
-  const request = new NextRequest('http://localhost:3000/api/songs');
-  const response = await GET(request);
-  const body = await response.json();
+  test("returns status 400 when mood parameter is missing", async () => {
+    const request = new NextRequest('http://localhost:3000/api/songs');
+    const response = await GET(request);
+    const body = await response.json();
 
-  expect(response.status).toBe(400);
-  expect(body).toEqual({ error: "Mood parameter is required" });
+    expect(response.status).toBe(400);
+    expect(body).toEqual({ error: "Mood parameter is required" });
+  })
+
+  test("returns status 200 when a mood is provided", async () => {
+    const request = new NextRequest('http://localhost:3000/api/songs?mood=happy');
+    const response = await GET(request);
+    const body = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(body).toEqual({ songs: [] })
+  })
 })
 
-test("GET api/songs returns status 200 when a mood is provided", async () => {
-  const request = new NextRequest('http://localhost:3000/api/songs?mood=happy');
-  const response = await GET(request);
-  const body = await response.json();
 
-  expect(response.status).toBe(200);
-  expect(body).toEqual({ songs: [] })
-})
