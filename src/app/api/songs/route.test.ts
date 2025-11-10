dea994ce7ca456ee7a22a752fb9f37a27ab3df90
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-10 14:09:52 -0300

    unit-blue

--- src/app/api/songs/route.test.ts
+++ src/app/api/songs/route.test.ts
@@ -25,10 +25,8 @@ describe("GET api/songs", () => {
   test("returns status 200 when a mood is provided", async () => {
     const request = new NextRequest('http://localhost:3000/api/songs?mood=happy');
     const response = await GET(request);
-    const body = await response.json();
 
     expect(response.status).toBe(200);
-    expect(body).toEqual({ songs: [] })
   })
 })
 
