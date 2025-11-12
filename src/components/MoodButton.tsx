a42713614a47c4f7e012fa11041bb7b813748f05
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-12 16:08:17 -0300

    unit-green

--- src/components/MoodButton.tsx
+++ src/components/MoodButton.tsx
@@ -1,5 +1,10 @@
+import Image from 'next/image'
+
 export default function MoodButton({ mood, onClick }) {
   return (
-    <></>
+    <button>
+      <Image src={mood.emoji} alt={mood.label} width={100} height={100} />
+      <span>{mood.label}</span>
+    </button>
   )
 }
