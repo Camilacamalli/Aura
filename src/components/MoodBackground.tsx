15c2d097df69d5eb97218399c17d7429ce3083da
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-17 15:46:29 -0300

    unit-green

--- src/components/MoodBackground.tsx
+++ src/components/MoodBackground.tsx
@@ -1,4 +1,13 @@
-export default function MoodBackground() {
+type MoodBackgroundProps = {
+  mood?: string
+};
+
+export default function MoodBackground({ mood }: MoodBackgroundProps) {
+  if (mood === 'very sad') {
+    return (
+      <div data-testid="mood-background-very-sad"></div>
+    )
+  }
   return (
     <div data-testid="mood-background-default"></div>
   )
