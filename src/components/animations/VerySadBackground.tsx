110bc6fcf4c861b65a7cecb7a7a2df30d270fd90
Camila Camalli <camilaagustinacamalli@gmail.com>
2025-11-18 15:45:37 -0300

    unit-green

--- src/components/animations/VerySadBackground.tsx
+++ src/components/animations/VerySadBackground.tsx
@@ -2,7 +2,10 @@ export default function VerySadBackground() {
   return (
     <div data-testid="mood-background-very-sad" className="absolute inset-0 -z-10">
       <div className="w-full h-full bg-gray-200 opacity-50">
-        <div data-testid="raindrops-container"></div>
+        <div data-testid="raindrops-container">
+          <div data-testid="raindrop"></div>
+          <div data-testid="raindrop"></div>
+        </div>
       </div>
     </div>
   )
